c88411d34442df3fbb86eee6080c5082f347ce8a
Alexander <alexander.pankov88@mail.ru>
2021-05-05 22:16:43 +0700

    move 'Film' and 'show more' to separate presenter

--- src/main.js
+++ src/main.js
@@ -2,18 +2,8 @@ import HeaderProfileView from './view/profile.js';
 import MainNavView from './view/main-nav.js';
 import FilterView from './view/main-nav-filter.js';
 import NavStatsView from './view/main-nav-stats.js';
-import SortingView from './view/sorting.js';
-import FilmHolderView from './view/films.js';
-import FilmsListView from './view/films-list.js';
-import FilmsListTopView from './view/films-list-top.js';
-import FilmsListCommentedView from './view/films-list-commented.js';
-import FilmView from './view/film-card.js';
-import NoFilmView from './view/no-film.js';
-import ShowMoreView from './view/show-more.js';
-import PopupView from './view/popup.js';
 import FooterStatsView from './view/footer-stats.js';
-import {sortFilmsByRating, sortFilmsByComments} from './utils/film.js';
-import {render, RenderPosition, remove} from './utils/render.js';
+import {render, RenderPosition} from './utils/render.js';
 
 import FilmListPresenter from './presenter/film-list.js';
 
@@ -25,9 +15,9 @@ import {generateFilter} from './mock/filter.js';
 import {generateFooterStats} from './mock/footer-stats.js';
 
 const FILM_COUNT = 20;
-const FILM_COUNT_PER_STEP = 5;
-const TOP_RATED_COUNT = 2;
-const COMMENTED_COUNT = 2;
+// const FILM_COUNT_PER_STEP = 5;
+// const TOP_RATED_COUNT = 2;
+// const COMMENTED_COUNT = 2;
 
 const comments = generateCommentsList();
 const films = generateFilms(FILM_COUNT, comments);
--- src/presenter/film-list.js
+++ src/presenter/film-list.js
@@ -1,13 +1,14 @@
 import SortingView from '../view/sorting.js';
-import FilmView from '../view/film-card.js';
+// import FilmView from '../view/film-card.js';
 import NoFilmView from '../view/no-film.js';
 import FilmHolderView from '../view/films.js';
 import FilmsListView from '../view/films-list.js';
 import FilmsListInnerView from '../view/films-list-inner.js';
 import ShowMoreView from '../view/show-more.js';
-import FilmsListTopView from '../view/films-list-top.js';
-import FilmsListCommentedView from '../view/films-list-commented.js';
-import PopupView from '../view/popup.js';
+// import FilmsListTopView from '../view/films-list-top.js';
+// import FilmsListCommentedView from '../view/films-list-commented.js';
+import FilmPresenter from './film.js';
+
 import {render, RenderPosition, remove} from '../utils/render.js';
 
 const FILM_COUNT_PER_STEP = 5;
@@ -15,12 +16,16 @@ const FILM_COUNT_PER_STEP = 5;
 export default class FilmList {
   constructor(filmListContainer) {
     this._filmListContainer = filmListContainer;
+    this._renderedFilmCount = FILM_COUNT_PER_STEP;
 
     this._filmHolder = new FilmHolderView();
     this._filmListComponent = new FilmsListView();
     this._filmListInnerComponent = new FilmsListInnerView();
     this._sortComponent = new SortingView();
     this._noFilmComponent = new NoFilmView();
+    this._showMoreButtonComponent = new ShowMoreView();
+
+    this._handleShowMoreButtonClick = this._handleShowMoreButtonClick.bind(this);
   }
 
   init(films, comments) {
@@ -38,35 +43,9 @@ export default class FilmList {
     render(this._filmHolder, this._sortComponent, RenderPosition.BEFOREBEGIN);
   }
 
-  _renderFilm(film, comments) {
-    const filmComponent = new FilmView(film);
-
-    const closePopupHandler = (element) => {
-      document.body.classList.remove('hide-overflow');
-      document.body.removeChild(element.getElement());
-    };
-
-    filmComponent.setItemsClickHandler(() => {
-      document.body.classList.add('hide-overflow');
-      const filmPopup = new PopupView(film, this._comments);
-
-      filmPopup.setCloseClickHandler(() => {
-        closePopupHandler(filmPopup);
-      });
-
-      const onEscKeyDown = (evt) => {
-        if (evt.key === 'Escape' || evt.key === 'Esc') {
-          evt.preventDefault();
-          closePopupHandler(filmPopup);
-          document.removeEventListener('keydown', onEscKeyDown);
-        }
-      };
-      document.addEventListener('keydown', onEscKeyDown);
-
-      document.body.appendChild(filmPopup.getElement());
-    });
-
-    render(this._filmListInnerComponent, filmComponent, RenderPosition.BEFOREEND);
+  _renderFilm(film) {
+    const filmPresenter = new FilmPresenter(this._filmListInnerComponent);
+    filmPresenter.init(film, this._comments);
   }
 
   _renderFilms(from, to) {
@@ -79,23 +58,19 @@ export default class FilmList {
     render(this._filmListContainer, this._noFilmComponent, RenderPosition.BEFOREEND);
   }
 
+  _handleShowMoreButtonClick() {
+    this._renderFilms(this._renderedFilmCount, this._renderedFilmCount + FILM_COUNT_PER_STEP);
+    this._renderedFilmCount += FILM_COUNT_PER_STEP;
+
+    if (this._renderedFilmCount >= this._films.length) {
+      remove(this._showMoreButtonComponent);
+    }
+  }
+
   _renderShowMoreButton() {
-    let renderedFilmsCount = FILM_COUNT_PER_STEP;
+    render(this._filmListComponent, this._showMoreButtonComponent, RenderPosition.BEFOREEND);
 
-    const showMoreButtonComponent = new ShowMoreView();
-    render(this._filmListComponent, showMoreButtonComponent, RenderPosition.BEFOREEND);
-
-    showMoreButtonComponent.setClickHandler(() => {
-      this._films
-        .slice(renderedFilmsCount, renderedFilmsCount + FILM_COUNT_PER_STEP)
-        .forEach((film) => this._renderFilm(film));
-
-      renderedFilmsCount += FILM_COUNT_PER_STEP;
-
-      if (renderedFilmsCount >= this._films.length) {
-        remove(showMoreButtonComponent);
-      }
-    });
+    this._showMoreButtonComponent.setClickHandler(this._handleShowMoreButtonClick);
   }
 
   _renderFilmItems() {
